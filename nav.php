43797cdb13d65c1f1c9283b10c4365d307dacd34
Daniel Rahn <danrahn@uw.edu>
2019-12-01 10:47:20 -0800

    Allow middle mouse clicks on nav buttons to open page in a new tab

--- nav.php
+++ nav.php
@@ -6,17 +6,17 @@
         <div class="ham"></div>
     </div>
     <div id="navShort">
-        <div class="navButton rightbutton" onclick="window.location = 'logout.php'">
+        <div class="navButton rightbutton" id="navLogoutTop">
             <div class=btnimg><image src='resource/logout.png' alt='Logout' style='filter: invert(80%);margin-top:8px'/></div>
         </div>
-        <div class="navButton rightbutton"onclick="window.location = 'requests.php'">
+        <div class="navButton rightbutton" id="navRequestsTop">
             <div class=btnimg><image src='resource/requests.png' alt='Requests' style='filter: invert(80%);margin-top:8px'/></div>
         </div>
-        <div class="navButton rightbutton" onclick="window.location = 'user_settings.php'">
+        <div class="navButton rightbutton" id="navSettingsTop">
             <div class=btnimg><image src='resource/settings.png' alt='Settings' style='filter: invert(80%);margin-top:8px'/></div>
         </div>
     </div>
-    <button id="pageName" class="navPageInfo" onclick="window.location = 'index.php'">Plex Web</button>
+    <button id="pageName" class="navPageInfo">Plex Web</button>
     <?php
         if ($_SERVER['REQUEST_URI'] != '/plexweb/index.php' &&
             $_SERVER['REQUEST_URI'] != '/plexweb/')
@@ -28,41 +28,41 @@
     ?>
     <?php if ($location == "Request") {?>
     <div class="navPageInfo">&#x2192;</div>
-    <button class="navPageInfo pageDetail" onclick="window.location = 'requests.php'">Requests</button>
+    <button class="navPageInfo pageDetail" id="backToRequests">Requests</button>
     <?php } ?>
     <div class="navPageInfo">&#x2192;</div>
-    <button class="navPageInfo pageDetail" onclick="window.location = '<?= $_SERVER['REQUEST_URI'] ?>'"><?= $location ?></button>
+    <button class="navPageInfo pageDetail" id="currentPage"><?= $location ?></button>
     <?php } ?>
 </div>
 </div>
 <div id="leftMenu">
-    <div class="navButton" onclick="window.location = 'index.php'">
+    <div class="navButton" id="navHome">
         <button class=btntxt>Home</button>
         <div class=btnimg><image src='resource/home.png' alt='Home' style='filter: invert(80%);'/></div>
     </div>
-    <div class="navButton rightbutton" onclick="window.location = 'new_request.php'">
+    <div class="navButton rightbutton" id="navNewRequest">
         <button class=btntxt>New Request</button>
         <div class=btnimg><image src='resource/new_request.png' alt='NewRequest' style='filter: invert(80%);'/></div>
     </div>
 <?php if (isset($_SESSION['level']) && (int)$_SESSION['level'] >= 100) { ?>
-    <div class="navButton" onclick="window.location = 'members.php'">
+    <div class="navButton" id="navMembers">
         <button class=btntxt>Members</button>
         <div class=btnimg><image src='resource/members.png' alt='Home' style='filter: invert(80%);'/></div>
     </div>
 <?php } ?>
-    <div class="navButton rightbutton" onclick="window.location = 'user_settings.php'">
+    <div class="navButton rightbutton" id="navUserSettings">
         <button class=btntxt>Settings</button>
         <div class=btnimg><image src='resource/settings.png' alt='Settings' style='filter: invert(80%);'/></div>
     </div>
-    <div class="navButton" onclick="window.location = 'requests.php'">
+    <div class="navButton" id="navRequests">
         <button class=btntxt>Requests</button>
         <div class=btnimg><image src='resource/requests.png' alt='Requests' style='filter: invert(80%);'/></div>
     </div>
-    <div class="navButton" onclick="window.location = 'logout.php'">
+    <div class="navButton" id="navLogout">
         <button class=btntxt>Logout</button>
         <div class=btnimg><image src='resource/logout.png' alt='Logout' style='filter: invert(80%);'/></div>
     </div>
-    <div class="navButton" onclick="window.open('https://github.com/danrahn/plexweb', '_blank')">
+    <div class="navButton" id="navGithub">
         <button class=btntxt>Source Code</button>
         <div class=btnimg><image src='resource/github.png' alt='Github' style='filter: invert(80%);'/></div>
     </div>
@@ -81,6 +81,49 @@
             }
         }
     });
+
+    setupClicks("pageName", "index.php");
+    setupClicks("backToRequests", "requests.php");
+    setupClicks("currentPage", "<?= $_SERVER['REQUEST_URI'] ?>");
+
+    setupClicks("navLogoutTop", "logout.php");
+    setupClicks("navRequestsTop", "requests.php");
+    setupClicks("navSettingsTop", "user_settings.php");
+
+    setupClicks("navHome", "index.php");
+    setupClicks("navNewRequest", "new_request.php");
+    setupClicks("navMembers", "members.php");
+    setupClicks("navUserSettings", "user_settings.php");
+    setupClicks("navRequests", "requests.php");
+    setupClicks("navLogout", "logout.php");
+    setupClicks("navGithub", "https://github.com/danrahn/plexweb", true);
+
+
+    function setupClicks(id, url, forceNewWindow)
+    {
+        let element = document.getElementById(id);
+        if (!element)
+        {
+            return;
+        }
+
+        if (forceNewWindow)
+        {
+            element.addEventListener("click", function(e) {
+                window.open(url, "_blank");
+            });
+        }
+        else
+        {
+            element.addEventListener("click", function(e) {
+                window.location = url;
+            });
+        }
+
+        element.addEventListener("auxclick", function(e) {
+            window.open(url, "_blank");
+        });
+    }
 
     window.addEventListener("keydown", function(e) {
         e = e || window.event;
