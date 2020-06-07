b4430a6d5599fa6f7e2f647110966160c0265e33
Daniel Rahn <danrahn@uw.edu>
2020-06-06 17:33:05 -0700

    Add unshortener

---
+++ unshorten.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once "includes/common.php";
+require_once "includes/config.php";
+
+session_start();
+requireSSL();
+
+$type = try_get('type');
+$data = try_get('data');
+if (!$type || !$data)
+{
+    header("Location: /plex/index.php");
+    exit;
+}
+
+switch ($type)
+{
+    case 'r':
+        header('Location: /plex/request.php?id=' . $data);
+        exit;
+    default:
+        header('Location: /plex/index.php');
+        break;
+}
+
+?>
